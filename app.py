97f217628c99394c63fa4f4a85414eeced6bc24a
Christian Casey <christiancasey86@gmail.com>
2020-06-03 23:37:26 -0400

    Fixing heroku issue

--- app.py
+++ app.py
@@ -247,9 +247,9 @@ def render_bagnall_template(items, count):
             numbered_items.sort()
             categories[i]['items'] = [ ni.item for ni in numbered_items ]
             
-    return render_template('isaw-bibliography.html', title='Author: bagnall', items=items, count=count)
+    # return render_template('isaw-bibliography.html', title='Author: bagnall', items=items, count=count)
     
-    # return render_template('isaw-bibliography-bagnall.html', title='Author: bagnall', items=items, categories=categories, count=count)
+    return render_template('isaw-bibliography-bagnall.html', title='Author: bagnall', categories=categories, count=count)
 
 
 @app.route('/author/<author>')
